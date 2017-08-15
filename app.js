577fc159a64bc4b7a4b5a1f773e8bcf1f69e5d80
Timm Jensen <4342842@rts-365.dk>
2017-08-15 12:31:43 +0200

    Opgaven variabler - lavet færdig

--- app.js
+++ app.js
@@ -1,1 +1,79 @@
 console.log("Hej stuff!");
+
+var antal = 5;
+var prisPerStyk = 10;
+var pris = antal * 10;
+
+console.log('Prisen per styk er '+ prisPerStyk + " kr.");
+
+// Variabler
+// Øvelse 1
+var navn = "Benny";
+var alder = 19;
+var tekst = `Mit navn er ${navn} og jeg er ${alder} år gammel`;
+
+console.log(tekst);
+
+// Øvelse 2
+var moms = 1.25;
+var pris_uden_moms = 400;
+
+var pris_med_moms = moms * pris_uden_moms;
+var pris_uden_moms_02 = pris_med_moms / moms;
+
+console.log(pris_uden_moms_02);
+
+// Øvelse 3
+
+var forste = 10;
+var anden = 20;
+
+anden = anden - forste;
+forste = anden + anden;
+
+console.log(forste);
+console.log(anden);
+
+// Øvelse 4.1
+
+var carName = "Volvo";
+
+console.log(carName);
+
+// Øvelse 4.2
+
+var number = 50;
+
+console.log(number);
+
+// Øvelse 4.3
+
+var carName = "Volvo";
+console.log(carName);
+
+// Øvelse 4.4
+
+var x = 5;
+var y = 10;
+console.log(x + y);
+
+// Øvelse 4.5
+
+var z = x + y;
+
+console.log(z);
+
+// Øvelse 4.6
+
+var firstName = "John", lastName = "Doe", age = 35;
+
+// Arrays
+// Øvelse 1.1
+
+var cars = ["Saab", "Volvo", "BMW"];
+
+console.log(cars);
+
+// Øvelse 1.2
+
+console.log(cars 1);
