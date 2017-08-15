94659d31566c19c008cdb1b4729d516409b5875b
Timm Jensen <4342842@rts-365.dk>
2017-08-15 14:02:01 +0200

    rettede opgave 2
    rettede opgave 2

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ console.log(tekst);
 var moms = 1.25;
 var pris_uden_moms = 400;
 
-var pris_med_moms = moms * pris_uden_moms;
+var pris_med_moms = pris_uden_moms * moms;
 var pris_uden_moms_02 = pris_med_moms / moms;
 
 console.log(pris_uden_moms_02);
