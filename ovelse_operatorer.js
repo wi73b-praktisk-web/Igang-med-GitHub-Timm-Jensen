2f39fe0d3d4ea5fc742cdd823084c1c732c192d1
Timm Jensen <4342842@rts-365.dk>
2017-08-16 14:46:40 +0200

    tja
    tja

---
+++ ovelse_operatorer.js
@@ -0,0 +1,39 @@
+// Øvelse 1
+var tal1 = 3;
+var tal2 = 7;
+
+console.log(tal1 + tal2);
+console.log(tal1 - tal2);
+console.log(tal1 * tal2);
+console.log(tal1 / tal2);
+console.log(tal1 % tal2);
+console.log(++tal1);
+console.log(tal1++);
+console.log(tal1);
+
+// Øvelse 2
+
+var tal = 12;
+var tel = 22;
+
+tal += 5;
+console.log(tal);
+
+tal -= 5;
+console.log(tal);
+
+tel *= 2;
+console.log(tel);
+
+tel /= 2;
+console.log(tel);
+
+tal %= 122;
+console.log(tal);
+
+// Øvelse 3
+var tal = 2;
+
+if (3 -= 1) {
+    console.log(tal);
+}
