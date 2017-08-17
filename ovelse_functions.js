8f731037e1e96a0c20fb348a69db5d7370797f28
Timm Jensen <4342842@rts-365.dk>
2017-08-17 10:25:59 +0200

    Tilføjer øvelse 1 og 2 samt Anonyme functioner

--- ovelse_functions.js
+++ ovelse_functions.js
@@ -31,3 +31,34 @@ udskriv('Hans', 'hansen', 2000);
 
 
 // Øvelse B
+
+function add1(tal1, tal2) {
+    console.log(tal1 + tal2);
+}
+
+add1(3, 5);
+
+var result = 0;
+function add(tal) {
+    // result = result + tal;    der er en nemmere måde at skrive dette på: result += tal;
+    result += tal;
+    console.log(result);
+}
+
+function sub(tal) {
+    result -= tal;
+    console.log(result);
+}
+
+add(8);
+add(5);
+sub(3);
+
+// Anonyme functioner
+// Anonym funktion
+var sayHeyAnonym = function (value) {
+    console.log(value); // Udskriver "test af anonym function"
+}
+
+sayHeyAnonym('test af anonym funktion');
+
