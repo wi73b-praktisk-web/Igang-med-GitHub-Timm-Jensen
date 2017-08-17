e1f6ef07a283ec2e1706507881f87af8d952bb07
Timm Jensen <4342842@rts-365.dk>
2017-08-17 09:30:09 +0200

    test commit fra mac vsc

---
+++ ovelse_functions.js
@@ -0,0 +1,43 @@
+hello(); // 
+
+function hello() {
+    console.log("Helloooooooo");
+}
+
+
+function sayHello(who) { // Who er et argument
+    console.log('hello', who);
+}
+
+sayHello('hoisting'); // Udskriver "hello hoisting"
+sayHello('me'); // Udskriver "hello me"
+
+
+// Hoisting virker ikke altid.
+
+var sayHey = function sayHey(value) {
+    console.log(value);
+}
+sayHey('test for hoisting?');
+
+
+// Øvelse A
+
+function udskriv(fornavn, efternavn, fodselsdag) {
+    console.log(fornavn, efternavn, fodselsdag);
+}
+
+udskriv('Hans', 'hansen', 2000);
+
+
+// Øvelse B
+
+printTal();
+function printTal() {
+    console.log( 7 + 7);
+    console.log( 7 - 7);
+    console.log( 7 * 7);
+    console.log( 7 / 7);
+    console.log( 7 % 7);
+
+}
