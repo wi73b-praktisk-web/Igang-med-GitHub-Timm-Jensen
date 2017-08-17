6da887243a451038a8238ecc75544450ada8d4ab
Timm Jensen <4342842@rts-365.dk>
2017-08-17 12:59:54 +0200

    added alot of functions

--- ovelse_functions.js
+++ ovelse_functions.js
@@ -55,10 +55,83 @@ add(5);
 sub(3);
 
 // Anonyme functioner
-// Anonym funktion
+// Anonym function
+// Gammel måde at skrive functioner på
 var sayHeyAnonym = function (value) {
     console.log(value); // Udskriver "test af anonym function"
 }
 
 sayHeyAnonym('test af anonym funktion');
 
+// Anden måde at skrive functioner på
+var sayHeyAnonym = (value) => {  // Erstat order "function" med en pil: => og hvis man kun har 1 argument, behøver man ikke at skrive "()"
+    console.log(value); // Udskriver "test af anonym function"
+}
+
+sayHeyAnonym('test af anonym funktion');
+
+// Ny måde at skrive functioner på
+var sayWhat = value => console.log(value); 
+
+sayWhat('test af anonym funktion');
+
+// Arguments og default-values
+function add(x = 0, y = 0) {
+    console.log(`x er lig med ${x} og y=${y}. Sum: `, x + y);
+}
+ 
+add(); //Udskriver "x er lig med 0 og y=0. Sum:  0"
+add(3, 8); //Udskriver "x er lig med 3 og y=8. Sum:  11"
+
+// Øvelse 2
+
+var langTekt = "Dette er en meget lang tekst som skal afkortes ved hjælp af en function som jeg skriver nedenfor";
+
+function afkortstreng(tekst) {
+    return tekst.substr(0, 28); // .substr() er det samme som at skrive .substring()
+
+    // return tekst.substring(0, 28); .substr() er det samme som at skrive .substring()
+
+    // return tekst;
+}
+
+console.log(afkortstreng(langTekt));
+
+// Øvelse 3
+// Argumenter kan være af typen array
+
+var dato = new Date();
+var birthday = new Date("21/04/95");
+var idol = ["Timm", "Jensen", 1995];
+var profil = function (p) {
+    return p[0] + " " + p[1] + "\n : Født " + p[2]; // Returnerer tekst-strengen "Timm Jensen: Født 1995"
+}
+
+console.log(dato.getFullYear() - birthday.getFullYear()); // Udskriver alder.
+console.log(profil(idol)); // Udskriver "Timm Jensen: Født 1995"
+
+// Selv øvelse: Opret en function, som fortæller hvor langt tid der er tilbage til juleaften.
+
+
+// Calculator - functions in a function
+function calculator() {
+    var result = 0;
+    return {
+        add: function (x) {
+            result += x;
+        },
+        sub: function (x) {
+            result  -= x;
+        },
+        result: function () {
+            return result;
+        }
+    }
+}
+
+var calc = calculator();
+calc.add(2);
+calc.add(5);
+calc.add(3);
+calc.sub(4);
+console.log(calc.result());
