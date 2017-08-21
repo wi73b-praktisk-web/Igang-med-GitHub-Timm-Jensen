79826cee37c4e67241c31127fa3fb4c16bcfe594
Timm Jensen <4342842@rts-365.dk>
2017-08-21 11:19:43 +0200

    øvelse 1 igang

---
+++ callback.js
@@ -0,0 +1,19 @@
+// // tænk på dette som den primære funktion der kaldes fra vores kode
+// function doSomething(someValue, callback){
+//     callback(someValue);
+//  }
+//  // denne funktion vil blive udført som en callback FRA doSomething funktionen
+//  function outputSomething(value){
+//     console.log(value);
+//  }
+
+//  doSomething('text', outputSomething);
+var data = [1, 2, 3, 4, 5, 6];
+
+function vejr(grader){
+    console.log(grader);
+};
+
+data.forEach(function(grader){
+    console.log(grader);
+});
