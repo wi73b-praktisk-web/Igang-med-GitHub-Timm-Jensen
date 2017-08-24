0c1bb617f8746a570beae23104dcfc251abb589b
Timm Jensen <4342842@rts-365.dk>
2017-08-24 13:07:46 +0200

    øvelse 1 & 3 added

--- ovelse_lokker.js
+++ ovelse_lokker.js
@@ -1,2 +1,17 @@
 // Løkker del 1
 
+var tal = 0;
+do {
+    console.log(`Dit nummer er: ${tal}`);
+    tal++;
+} while (tal <= 10);
+
+// øvelse 2
+
+
+// øvelse 3
+var i = new Date(2017);
+
+for (;i >= 1917;i--) {
+    console.log(i);
+};
