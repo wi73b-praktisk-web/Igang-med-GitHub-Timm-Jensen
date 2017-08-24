e24519a3a154c8fdfeae2da711259244a3a399f6
Timm Jensen <4342842@rts-365.dk>
2017-08-24 13:29:18 +0200

    løkker færdiggjort

--- ovelse_lokker.js
+++ ovelse_lokker.js
@@ -8,10 +8,25 @@ do {
 
 // øvelse 2
 
+for (var i = 1;i <= 25;i++) {
+    console.log(`i = ${i}`);
+};
+
+var x = 1;
+while (x <= 25){
+    console.log(`x = ${x}`);
+    x++;
+}
+
+var y = 1;
+do {
+    console.log(`y = ${y}`);
+    y++;
+} while (y <= 25);
+
 
 // øvelse 3
-var i = new Date(2017);
-
+var i = new Date(1/1/2017);
 for (;i >= 1917;i--) {
     console.log(i);
 };
